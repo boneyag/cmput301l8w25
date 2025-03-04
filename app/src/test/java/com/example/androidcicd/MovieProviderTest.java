c397e2773b870867a08e18fe70377207d465664f
akalankag <boney.ag@gmail.com>
2025-03-04 16:23:44 -0700

    initial mocktest for add movie

--- app/src/test/java/com/example/androidcicd/MovieProviderTest.java
+++ app/src/test/java/com/example/androidcicd/MovieProviderTest.java
@@ -1,4 +1,62 @@
 package com.example.androidcicd;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.androidcicd.movie.Movie;
+import com.example.androidcicd.movie.MovieProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class MovieProviderTest {
+    @Mock
+    private FirebaseFirestore mockFirestore;
+
+    @Mock
+    private CollectionReference mockMovieCollection;
+
+    @Mock
+    private DocumentReference mockDocRef;
+
+    private MovieProvider movieProvider;
+
+    @Before
+    public void setUp() {
+        // Start up mocks
+        MockitoAnnotations.openMocks(this);
+        // Define the behaviour we want during our tests. This part is what avoids the calls to firestore.
+        when(mockFirestore.collection("movies")).thenReturn(mockMovieCollection);
+        when(mockMovieCollection.document()).thenReturn(mockDocRef);
+        when(mockMovieCollection.document(anyString())).thenReturn(mockDocRef);
+        // Setup the movie provider
+        MovieProvider.setInstanceForTesting(mockFirestore);
+        movieProvider = MovieProvider.getInstance(mockFirestore);
+    }
+
+    @Test
+    public void testAddMovieSetsId() {
+        // Movie to add
+        Movie movie = new Movie("Oppenheimer", "Thriller/Historical Drama", 2023);
+
+        // Define the ID we want to set for the movie
+        when(mockDocRef.getId()).thenReturn("123");
+
+
+        // Add movie and check that we update our movie with the generated id
+        movieProvider.addMovie(movie);
+        assertEquals("Movie was not updated with correct id.", "123", movie.getId());
+
+    /*
+        Verify that we called the set method. Normally, this would call the database, but due to what we did in our setup method, this will not actually interact with the database, but the mock does track that the method was called.
+    */
+        verify(mockDocRef).set(movie);
+    }
 }
